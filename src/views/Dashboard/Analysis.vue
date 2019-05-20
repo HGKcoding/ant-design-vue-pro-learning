09e66e50ea563cb9f242077272cbd107bd19c027
huguokang <huguokang2017@gmail.com>
2019-05-20 14:16:21 +0800

    使用echarts

--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -1,12 +1,50 @@
 <template>
-  <div>分析页</div>
+  <div>
+    <Chart :option="chartOption" style="height: 400px" />
+  </div>
 </template>
 
 <script>
+import random from "lodash/random";
+import Chart from "../../components/Chart";
 export default {
   name: "Analysis",
+  components: {
+    Chart
+  },
   data() {
-    return {};
+    return {
+      chartOption: {
+        title: {
+          text: "ECharts 入门示例"
+        },
+        tooltip: {},
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [5, 10, 30, 20, 15, 55]
+          }
+        ]
+      }
+    };
+  },
+  mounted() {
+    this.interval = setInterval(() => {
+      this.chartOption.series[0].data = this.chartOption.series[0].data.map(
+        () => {
+          return random(100);
+        }
+      );
+      this.chartOption = { ...this.chartOption };
+    }, 3000);
+  },
+  beforeDestroy() {
+    clearInterval(this.interval);
   }
 };
 </script>
